5bedf39aa2da46e39afb45b7491da3090e18d96d
maymoonah-sulaiman <59207799+maymoonah-sulaiman@users.noreply.github.com>
2020-06-30 20:50:07 +0300

    migration

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 
-database_path = os.environ['DATABASE_URL']
+database_path = os.environ.get('DATABASE_URL', "postgresql://postgres:DGJ#%&@localhost:5432/sample")
 
 
 db = SQLAlchemy()
